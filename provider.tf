ee4ad3fe63cd900f6923415494b6c9dd72ed5416
ldohyunl <osolinnn@gmail.com>
2026-06-05 01:21:44 +0900

    AWS provider 버전 고정 (~> 5.80)

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
 # AWS 리전 및 공급자 설정
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.80"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
